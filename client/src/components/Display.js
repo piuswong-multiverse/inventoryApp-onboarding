5d0ef22f4892f4f8253d02f57bb3ce98272dc0d4
piuswong-multiverse <111539998+piuswong-multiverse@users.noreply.github.com>
2022-09-09 18:23:17 -0500

    React UI catches error on fetch

--- client/src/components/Display.js
+++ client/src/components/Display.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 const Display = ( { view }) => {
 
-    const [items, setItems] = useState([]);
+    const [items, setItems] = useState({});
 
     const displayCategories = (categories) => { // takes Category object as argument
         return categories.map( (category) => {
@@ -11,16 +11,17 @@ const Display = ( { view }) => {
     };
 
     const getAllItems = async () => {
-        const response = await fetch('/api/items/');
-        // // const response = await fetch('http://localhost:3001/api/items/'); // this version would bring up CORS errors on dev
-        const data = await response.json(); 
-        if(response.status !== 200){ // Show error if backend isn't working
-            setItems('Waiting for server');
-            throw Error(data.message)
+        try{
+            const response = await fetch('/api/items/');
+            // const response = await fetch('http://localhost:3001/api/items/'); // this version would bring up CORS errors on dev
+            const data = await response.json(); 
+            let items = data.allItems;
+            // console.log(items[0].name); // debug
+            setItems(items);
+        } catch (err) {
+            setItems({});
+            console.log(err)
         }
-        let items = data.allItems;
-        // console.log(items[0].name); // debug
-        setItems(items);
     }
 
     const displayItems = (items) => {
